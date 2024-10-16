a562ee4935269e4c2f5c7d3ed3de6b60f176651c
Eric Leonardis <leonardiseric@gmail.com>
2024-10-16 17:07:08 +0000

    custom actor, evaluator, feedforward actor for activations

--- vnl_ray/agents/policy_network_activations.py
+++ vnl_ray/agents/policy_network_activations.py
@@ -15,19 +15,21 @@ from acme.tf import networks as network_utils
 from dm_control import suite
 from acme import wrappers
 from vnl_ray.agents.utils_intention import separate_observation
+from vnl_ray.agents.actors import DelayedFeedForwardActor
+import tensorflow_probability as tfp
+import pandas as pd
 
 def _uniform_initializer():
   return tf.initializers.VarianceScaling(
       distribution='uniform', mode='fan_out', scale=0.333)
 
-class custom_actor(snt.Module):
+class CustomActorNetwork(snt.Module):
     def __init__(self, policy_network):
         # Initialize the parent snt.Module class
-        super(custom_actor, self).__init__(name="custom_actor")
+        super(CustomActorNetwork, self).__init__(name="CustomActorNetwork")
 
         self._policy_network = policy_network
         self._observation_network = separate_observation
-        self._samplehead = network_utils.StochasticSamplingHead()
 
     def __call__(self, inputs, return_activations=True):
         x = self._observation_network(inputs)
@@ -35,28 +37,75 @@ class custom_actor(snt.Module):
         x, activations = self._policy_network(x, return_activations=return_activations)
         #print(f"** ** ** activations: {activations}")
         #print(f"## ## ## OUTPUT OF POLICY NETWORK SHAPE # # # {x.shape}")  
-        x = self._samplehead(x)
         #print(f"## ## ## MEANHEAD OUPUT SHAPE  # # # {x.shape}")  
         return x, activations
     
-class custom_evaluator(snt.Module):
-    def __init__(self, policy_network):
-        # Initialize the parent snt.Module class
-        super(custom_evaluator, self).__init__(name="custom_evaluator")
+class ActivationsAwareFeedForwardActor(DelayedFeedForwardActor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.activations = None
+        self.activation_collection = []
+        self.activation_count = 0
+        self.episode_count = 0
+        self.df = None
+    
+    @tf.function
+    def _policy(self, observation: types.NestedTensor, return_activations: bool = True) -> types.NestedTensor:
+        
+        # Add a dummy batch dimension and as a side effect convert numpy to TF.
+        batched_observation = tf2_utils.add_batch_dim(observation)
 
-        self._policy_network = policy_network
-        self._observation_network = separate_observation
-        self._meanhead = network_utils.StochasticMeanHead()
+        policy, self.activations = self._policy_network(batched_observation, return_activations=True)
+
+        # Sample from the policy if it is stochastic.
+        action = policy.sample() if isinstance(policy, tfp.distributions.Distribution) else policy
+
+        return policy, self.activations
+
+    def select_action(self, observation: types.NestedArray) -> types.NestedArray:
+        action, self.activations = self._policy(observation, return_activations=True)
+        if self.activation_count < 50:
+            self.activation_collection.append(self.activations)
+            self.activation_count += 1
+
+        if self.activation_count == 50:
+            self.save_activations()
+            self.activation_count = 0
+            self.activation_collection.clear()
+            self.episode_count += 1
+
+        return tf2_utils.to_numpy_squeeze(action)
+    
+    def save_activations(self):
+        if self.df is None:
+            self.df = pd.DataFrame(columns=['layernorm_tanh', 'mlp_elu'])
+        
+        for activation in self.activation_collection:
+            self.df.loc[len(self.df)] = {'layernorm_tanh': activation['layernorm_tanh'].numpy(), 'mlp_elu': activation['mlp_elu'].numpy()}
+
+        # Write the accumulated DataFrame to an HDF5 file at the end of an episode
+        self.df.to_hdf('data.h5', key='activations', mode='a')
+
+class CustomEvaluatorNetwork(snt.Module):
+    def __init__(self, modules, name=None):
+        super(CustomEvaluatorNetwork, self).__init__(name="CustomEvaluatorNetwork")
+        self._modules = modules
+        self.activations = None
 
     def __call__(self, inputs, return_activations=True):
-        x = self._observation_network(inputs)
-        print(f"## ## ## INPUT TO POLICY NETWORK SHAPE # # # {x.shape}")  # Add this debug line
-        x, activations = self._policy_network(x, return_activations=return_activations)
-        #print(f"** ** ** activations: {activations}")
-        #print(f"## ## ## OUTPUT OF POLICY NETWORK SHAPE # # # {x.shape}")  
-        x = self._meanhead(x)
-        #print(f"## ## ## MEANHEAD OUPUT SHAPE  # # # {x.shape}")  
-        return x, activations
+        outputs = inputs
+        for module in self._modules:
+            print(type(module))
+
+        for module in self._modules:
+            if "IntermediateActivationsPolicyNetwork" in str(type(module)) and return_activations:
+                print("Activations module found!")
+                outputs, self.activations = module(outputs, return_activations=True)
+            else:
+                outputs = module(outputs)
+        if return_activations:
+            return outputs, self.activations
+        return outputs
 
 class IntermediateActivationsPolicyNetwork(snt.Module):
     def __init__(self, layer_sizes, num_dimensions, tanh_mean, init_scale, fixed_scale, use_tfd_independent):
@@ -90,12 +139,12 @@ class IntermediateActivationsPolicyNetwork(snt.Module):
     def __call__(self, inputs, return_activations=False):
         activations = dict()
 
-        #print(f"Before Linear Layer: {inputs.shape}")
+        #print(f"Input to policy network shape: {inputs.shape}, rank: {len(inputs.shape)}")
         x = self.linear(inputs)
         #print(f"After Linear Layer: {x.shape}")
 
         x = self.layer_norm(x)
-        print(f"After LayerNorm: {x.shape}")
+        #print(f"After LayerNorm: {x.shape}")
 
         x = tf.nn.tanh(x)
         activations["layernorm_tanh"] = x
