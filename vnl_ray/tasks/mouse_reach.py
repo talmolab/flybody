74db9c562827815a8defb901d4af69c488e05961
Eric Leonardis <leonardiseric@gmail.com>
2024-10-20 16:55:14 +0000

    add muscle simple

--- vnl_ray/tasks/mouse_reach.py
+++ vnl_ray/tasks/mouse_reach.py
@@ -1,6 +1,6 @@
 from dm_control import composer
 
-# import mouse entity 
+# import mouse entity
 from vnl_ray.mouse_forelimb.mouse_entity import MouseEntity
 from vnl_ray.tasks.arenas.mouse_arena import MouseReachArena
 from vnl_ray.tasks.mouse_reach_task import MouseReachTask
@@ -9,9 +9,12 @@ from dm_control import mjcf
 _CONTROL_TIMESTEP = 0.02
 _PHYSICS_TIMESTEP = 0.001
 
+
 def mouse_reach(random_state=None, actuator_type=None):
     if actuator_type == "muscle":
         mouse_xml_path = "/root/vast/eric/vnl-ray/vnl_ray/mouse_forelimb/assets_mousereach/armmodel_atscale_working_balljoint_muscle.xml"
+    elif actuator_type == "muscle_simple":
+        mouse_xml_path = "/root/vast/eric/vnl-ray/vnl_ray/mouse_forelimb/assets_mousereach/armmodel_atscale_working_balljoint_muscle_simple.xml"
     elif actuator_type == "torque":
         mouse_xml_path = "/root/vast/eric/vnl-ray/vnl_ray/mouse_forelimb/assets_mousereach/armmodel_atscale_working_balljoint_torque.xml"
     elif actuator_type == "position":
@@ -20,7 +23,7 @@ def mouse_reach(random_state=None, actuator_type=None):
         raise ValueError(f"Input Error: Input actuator type")
 
     # Initialize the MouseEntity without passing the physics object directly
-    mouse = MouseEntity(mouse_xml_path, 0.001)
+    mouse = MouseEntity(mouse_xml_path)
 
     # Create the arena
     arena = MouseReachArena()
@@ -35,7 +38,7 @@ def mouse_reach(random_state=None, actuator_type=None):
 
     # Return the environment
     return composer.Environment(
-        time_limit=3, 
+        time_limit=3,
         task=task,
         random_state=random_state,
         strip_singleton_obs_buffer_dim=True,
